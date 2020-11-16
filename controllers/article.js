82395321ed9c3375ea3bd9c4b9a1047bc6c5f39e
jose <joseluis19963@gmail.com>
2020-11-16 04:59:41 -0500

    search article method

--- controllers/article.js
+++ controllers/article.js
@@ -300,6 +300,42 @@ var controller = {
 
     }, // end getImage file
 
+    search: (req, res) => {
+
+        // Sacar el string a buscar
+        var searchString = req.params.search;
+
+        // Find or
+        Article.find({
+            "$or": [
+                { "title": { "$regex": searchString, "$options": "i" } },
+                { "content": { "$regex": searchString, "$options": "i" } }
+            ]
+        }).sort([['date', 'descending']]).exec((err, articles) => {
+
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error en la petición.'
+                });
+            }
+
+            if (!articles || articles.length <= 0) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay artículos que coincidan con tu busqueda.'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                articles
+            });
+
+        });
+
+    }, // end search
+
 }; // End controller
 
 module.exports = controller;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = 3900;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_blog', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb://localhost:27017/api_rest_blog', { useNewUrlParser: true, useUnifiedTopology: true, }).then(() => {
     console.log('Conexcion a la base de datos correcta !!!');
 
     // Crear servidor y ponerse a escuchar peticiones HTTP
